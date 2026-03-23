50babf5d56396b1fc90b2cfd9ea100d51fb92f1e
Vadzim <stubeda0002@mail.ru>
2026-03-23 19:42:18 +0500

    feature: add Hikari pool in DataSource

--- src/main/java/com/stubedavd/utils/DataSource.java
+++ src/main/java/com/stubedavd/utils/DataSource.java
@@ -1,12 +1,12 @@
 package com.stubedavd.utils;
 
-import com.stubedavd.exception.InfrastructureException;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
 import java.io.FileNotFoundException;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DataSource {
@@ -14,24 +14,40 @@ public class DataSource {
     private final static String DATABASE_URL = "jdbc:sqlite:";
     private final static String DATABASE_PATH = "/currencies.db";
 
-    private final String url;
+    private final HikariDataSource dataSource;
 
     public DataSource() {
         try {
             Class.forName(DRIVER);
             URL resource = DataSource.class.getResource(DATABASE_PATH);
-            if (resource == null) {
-                throw new FileNotFoundException(DATABASE_PATH);
-            }
-            this.url = DATABASE_URL + resource.toURI().getPath();
+            HikariConfig config = getHikariConfig(resource);
+
+            this.dataSource = new HikariDataSource(config);
         } catch (ClassNotFoundException | FileNotFoundException | URISyntaxException e) {
             throw new RuntimeException(e);
         }
     }
 
+    private HikariConfig getHikariConfig(URL resource) throws FileNotFoundException, URISyntaxException {
+        if (resource == null) {
+            throw new FileNotFoundException(DATABASE_PATH);
+        }
+        String jdbcUrl = DATABASE_URL + resource.toURI().getPath();
+
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(jdbcUrl);
+        config.setDriverClassName(DRIVER);
+        config.setMaximumPoolSize(10);
+        config.setMinimumIdle(2);
+        config.setConnectionTimeout(30000);
+        config.setIdleTimeout(60000);
+        config.setMaxLifetime(1800000);
+        return config;
+    }
+
     public Connection getConnection() {
         try {
-            return DriverManager.getConnection(url);
+            return dataSource.getConnection();
         } catch (SQLException e) {
             throw new RuntimeException();
         }
